36e599e6bcf8894d74b33e286ee5e85a247ed1c3
emotional001 <lwt2636134358@gmail.com>
2024-08-12 15:04:59 +0800

    拷贝整形数组

---
+++ javasepro/homework/homework/src/com/hudie/homework/Ticket6.java
@@ -0,0 +1,31 @@
+package com.hudie.homework;
+//复制一个整形的数组
+public class Ticket6 {
+    public static void main(String[] args) {
+        int[] arr ={11, 22, 33};
+        int[] arr2 = copy(arr);
+
+        printArray(arr2);
+    }
+
+    public static void printArray(int[] arr) {
+            System.out.print("[");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(i == arr.length - 1 ? arr[i] : arr[i] + ",");
+            }
+            System.out.print("]");
+    }
+
+    public static int[] copy(int[] arr){
+
+        int[] arr2 = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++){
+
+            arr2[i] = arr[i];
+
+        }
+        return arr2;
+    }
+
+}
