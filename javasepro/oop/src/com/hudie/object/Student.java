c0fc7c4f4b35794b5b9c37147b22817905c0bd95
emotional001 <lwt2636134358@gmail.com>
2024-08-12 19:19:51 +0800

    面对对象学生模板

---
+++ javasepro/oop/src/com/hudie/object/Student.java
@@ -0,0 +1,15 @@
+package com.hudie.object;
+//面对对象的学生成绩模板
+public class Student {
+    String name;
+    double chinese;
+    double math;
+
+    public void printTocalScore {
+        System.out.println(name + "的总成绩是： " + (chinese + math));
+    }
+
+    public void printAverageScore {
+        System.out.println(name + "的平均成绩是： " + (chinese + math) / 2.0);
+    }
+}
